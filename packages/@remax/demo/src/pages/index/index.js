ec731beb0a5817c37ba8050df9ee76497a02ad6a
Meck Zhu <yesmeck@gmail.com>
2019-06-26 01:02:24 +0800

    Use button in demo

--- packages/@remax/demo/src/pages/index/index.js
+++ packages/@remax/demo/src/pages/index/index.js
@@ -1,12 +1,12 @@
 import * as React from 'react';
-import { View } from '@remax/components';
+import { Button } from '@remax/components';
 import styles from './index.module.less';
 
 export default () => {
   const [count, setCount] = React.useState(0);
   return (
-    <View className={styles.text} onClick={() => setCount(count + 1)}>
+    <Button className={styles.btn} type="primary" onClick={() => setCount(count + 1)}>
       {count}
-    </View>
+    </Button>
   );
 };
