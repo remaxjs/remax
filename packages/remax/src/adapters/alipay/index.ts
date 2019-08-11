afc6d6e2267394fb3d213da05a4a155f0e3c1dee
caihuanyu <eterlf41@gmail.com>
2019-08-11 14:50:18 +0800

    fix: 修复导出错误

--- packages/remax/src/adapters/alipay/index.ts
+++ packages/remax/src/adapters/alipay/index.ts
@@ -3,5 +3,6 @@ export { default as createAppConfig } from '../../createAppConfig';
 export { default as createPageConfig } from '../../createPageConfig';
 export { PageProps } from '../../createPageWrapper';
 export { default as Platform } from '../../Platform';
+export * from '../../hooks';
 export * from './components';
 export * from './api';
--- packages/remax/src/adapters/wechat/index.ts
+++ packages/remax/src/adapters/wechat/index.ts
@@ -1,7 +1,8 @@
 export { default as render } from '../../render';
 export { default as createAppConfig } from '../../createAppConfig';
 export { default as createPageConfig } from '../../createPageConfig';
-export { PageProps, LifecycleHooks } from '../../createPageWrapper';
+export { PageProps } from '../../createPageWrapper';
 export { default as Platform } from '../../Platform';
+export * from '../../hooks';
 export * from './components';
 export * from './api';
