a555f16e025e39a726b1b63952fb22acaefe2fbe
caihuanyu <eterlf41@gmail.com>
2019-08-11 21:35:54 +0800

    fix: 修复 config.js 读取问题

--- packages/remax-cli/src/readManifest.ts
+++ packages/remax-cli/src/readManifest.ts
@@ -1,6 +1,6 @@
 export default function readManifest(path: string, target: string) {
   delete require.cache[require.resolve(path)];
-  const config = require(path) || require(path)[target];
+  const config = require(path)[target] || require(path);
 
   if (!config) {
     throw new Error(
