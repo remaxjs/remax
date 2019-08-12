617c0165efb92cbbc557642971f7eebc5c591321
caihuanyu <eterlf41@gmail.com>
2019-08-12 23:50:54 +0800

    feat: remax 网站
    resolve #69

---
+++ website/config.js
@@ -0,0 +1,33 @@
+const config = {
+  gatsby: {
+    pathPrefix: '/',
+    siteUrl: 'https://remaxjs.org',
+    gaTrackingId: null,
+  },
+  header: {
+    logo:
+      'https://gw.alipayobjects.com/mdn/rms_b5fcc5/afts/img/A*1NHAQYduQiQAAAAAAAAAAABkARQnAQ',
+    logoLink: 'https://remaxjs.org',
+    title: 'Remax',
+    githubUrl: 'https://github.com/remaxjs/remax',
+    helpUrl: '',
+    tweetText: '',
+    links: [{ text: '', link: '' }],
+  },
+  sidebar: {
+    forcedNavOrder: [''],
+    links: [],
+    frontline: false,
+    ignoreIndex: true,
+  },
+  siteMetadata: {
+    title: 'Remax',
+    description: 'Remax: 全新的小程序开发体验',
+    ogImage: null,
+    docsLocation: 'https://github.com/remaxjs/remax/tree/master/docs',
+    favicon:
+      'https://gw.alipayobjects.com/mdn/rms_b5fcc5/afts/img/A*1NHAQYduQiQAAAAAAAAAAABkARQnAQ',
+  },
+};
+
+module.exports = config;
