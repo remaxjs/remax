e1d35244d0849a02c06bd2fa92270cd7407e995f
caihuanyu <eterlf41@gmail.com>
2019-08-15 00:48:56 +0800

    fix(wechat): 修复微信端的问题

--- packages/remax/src/utils/promisify.ts
+++ packages/remax/src/utils/promisify.ts
@@ -5,18 +5,16 @@ function promisify(api: (config: any) => void) {
       api({
         ...config,
         success: (res: any) => {
-          if (typeof config.success === 'function') {
+          if (config && typeof config.success === 'function') {
             config.success(res);
-
-            resolve(res);
           }
+          resolve(res);
         },
         fail: (res: any) => {
-          if (typeof config.fail === 'function') {
+          if (config && typeof config.fail === 'function') {
             config.fail(res);
-
-            reject(res);
           }
+          reject(res);
         },
       });
     });
