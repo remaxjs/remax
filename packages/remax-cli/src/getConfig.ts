6d41398ed3fa14f20747fe8f212462c729fb35a4
insane <luojieyy@gmail.com>
2019-08-19 11:14:38 +0800

    feat: remax.config.js can export as function to tell current target

--- packages/remax-cli/src/getConfig.ts
+++ packages/remax-cli/src/getConfig.ts
@@ -9,10 +9,23 @@ export interface RemaxOptions {
   output: string;
 }
 
-export default function getConfig(): RemaxOptions {
+interface CliOptions {
+  target: string;
+}
+
+export default function getConfig(cli: CliOptions): RemaxOptions {
   const configPath: string = path.join(process.cwd(), './remax.config.js');
   if (fs.existsSync(configPath)) {
     const options = require(configPath);
+
+    // options 为 fucntion
+    if (options.prototype) {
+      return {
+        ...defaultOptions,
+        ...options(cli),
+      };
+    }
+
     return {
       ...defaultOptions,
       ...options,
