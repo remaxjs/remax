421ab84b3c502a4fdfbb3c1fd54c0c2c4fb4c50b
Wei Zhu <yesmeck@gmail.com>
2019-08-21 00:31:13 +0800

    chore: config eslint with prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,8 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
   ],
   globals: {
     App: true,
