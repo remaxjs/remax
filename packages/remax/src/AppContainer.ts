9b5bc84192eac4911d3baf8b94c02ae722d38ab9
Wei Zhu <yesmeck@gmail.com>
2019-08-31 02:42:34 +0800

    feat: 支持 App 是一个 React 组件

---
+++ packages/remax/src/AppContainer.ts
@@ -0,0 +1,53 @@
+import VNode, { Path, RawNode } from './VNode';
+import { generate } from './instanceId';
+import { FiberRoot } from 'react-reconciler';
+
+interface SpliceUpdate {
+  path: Path;
+  start: number;
+  deleteCount: number;
+  items: RawNode[];
+}
+
+export default class AppContainer {
+  context: any;
+  root: VNode;
+  updateQueue: SpliceUpdate[] = [];
+  _rootContainer?: FiberRoot;
+
+  constructor(context: any) {
+    this.context = context;
+
+    this.root = new VNode({
+      id: generate(),
+      type: 'root',
+      container: this,
+    });
+    this.root.mounted = true;
+  }
+
+  requestUpdate(
+    path: Path,
+    start: number,
+    deleteCount: number,
+    ...items: RawNode[]
+  ) {
+    // ignore
+  }
+
+  createCallback(name: string, fn: Function) {
+    this.context[name] = fn;
+  }
+
+  appendChild(child: VNode) {
+    this.root.appendChild(child);
+  }
+
+  removeChild(child: VNode) {
+    this.root.removeChild(child);
+  }
+
+  insertBefore(child: VNode, beforeChild: VNode) {
+    this.root.insertBefore(child, beforeChild);
+  }
+}
--- packages/remax/src/createPageConfig.ts
+++ packages/remax/src/createPageConfig.ts
@@ -3,34 +3,45 @@ import createPageWrapper from './createPageWrapper';
 import render from './render';
 import { Lifecycle, callbackName } from './lifecycle';
 import Container from './Container';
+import { createPortal } from './ReactPortal';
+
+let idCounter = 0;
 
 export default function createPageConfig(Page: React.ComponentType<any>) {
+  const app = getApp();
+  const id = idCounter;
+  idCounter += 1;
+
   return {
+    pageId: 'page_' + id,
+
     data: {
       action: {},
     },
 
-    wrapper: null as any,
+    wrapperRef: React.createRef<any>(),
 
     lifecycleCallback: {} as any,
 
     onLoad(this: any, query: any) {
       const PageWrapper = createPageWrapper(Page, query);
       this.container = new Container(this);
-
-      this.wrapper = render(
-        React.createElement(PageWrapper, { page: this }),
-        this.container
+      this.element = createPortal(
+        React.createElement(PageWrapper, {
+          page: this,
+          ref: this.wrapperRef,
+        }),
+        this.container,
+        this.pageId
       );
+
+      app._mount(this);
     },
 
     onUnload(this: any) {
       this.unloaded = true;
-      if (this.requestUpdate) {
-        this.requestUpdate.clear();
-      }
-      render(null, this.container);
-      this.wrapper = null;
+      this.container.clearUpdate();
+      app._unmount(this);
     },
 
     /**
@@ -61,8 +72,8 @@ export default function createPageConfig(Page: React.ComponentType<any>) {
       }
 
       const callback = callbackName(lifecycle);
-      if (this.wrapper[callback]) {
-        return this.wrapper[callback](...args);
+      if (this.wrapperRef.current && this.wrapperRef.current[callback]) {
+        return this.wrapperRef.current[callback](...args);
       }
     },
 
