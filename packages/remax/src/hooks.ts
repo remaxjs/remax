3b5e039d21b46bdbbd137979e89a821ec09c9e70
Wei Zhu <yesmeck@gmail.com>
2019-09-02 19:59:45 +0800

    fix: 修复生命周期 hooks 里的闭包变量不更新的问题

--- packages/remax/src/hooks.ts
+++ packages/remax/src/hooks.ts
@@ -4,31 +4,31 @@ import { registerLifecycle, Lifecycle, Callback } from './lifecycle';
 export function useShow(callback: Callback) {
   useLayoutEffect(() => {
     return registerLifecycle(Lifecycle.show, callback);
-  }, []);
+  });
 }
 
 export function useHide(callback: Callback) {
   useLayoutEffect(() => {
     return registerLifecycle(Lifecycle.hide, callback);
-  }, []);
+  });
 }
 
 export function usePullDownRefresh(callback: Callback) {
   useLayoutEffect(() => {
     return registerLifecycle(Lifecycle.pullDownRefresh, callback);
-  }, []);
+  });
 }
 
 export function useReachBottom(callback: Callback) {
   useLayoutEffect(() => {
     return registerLifecycle(Lifecycle.reachBottom, callback);
-  }, []);
+  });
 }
 
 export function usePageScroll(callback: Callback) {
   useLayoutEffect(() => {
     return registerLifecycle(Lifecycle.pageScroll, callback);
-  }, []);
+  });
 }
 
 export function useShareAppMessage(
@@ -40,29 +40,29 @@ export function useShareAppMessage(
 ) {
   useLayoutEffect(() => {
     return registerLifecycle(Lifecycle.shareAppMessage, callback);
-  }, []);
+  });
 }
 
 export function useTitleClick(callback: Callback) {
   useLayoutEffect(() => {
     return registerLifecycle(Lifecycle.titleClick, callback);
-  }, []);
+  });
 }
 
 export function useOptionMenuClick(callback: Callback) {
   useLayoutEffect(() => {
     return registerLifecycle(Lifecycle.optionMenuClick, callback);
-  }, []);
+  });
 }
 
 export function usePopMenuClick(callback: Callback) {
   useLayoutEffect(() => {
     return registerLifecycle(Lifecycle.popMenuClick, callback);
-  }, []);
+  });
 }
 
 export function usePullIntercept(callback: Callback) {
   useLayoutEffect(() => {
     return registerLifecycle(Lifecycle.pullIntercept, callback);
-  }, []);
+  });
 }
