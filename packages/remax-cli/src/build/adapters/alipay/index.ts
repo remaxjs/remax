0b4ce79dee8bb7a5758d535364d51090494af93e
Pochodaydayup <13343460801@163.com>
2019-09-10 21:15:46 +0800

    feat: 修改支付宝和微信适配原生组件

--- packages/remax-cli/src/build/adapters/alipay/index.ts
+++ packages/remax-cli/src/build/adapters/alipay/index.ts
@@ -18,7 +18,6 @@ export const extensions = {
 const templateBaseDir = path.join(__dirname, '../../../../templates');
 
 export const templates = {
-  base: path.join(templateBaseDir, 'alipay/base.ejs'),
   component: path.join(templateBaseDir, 'alipay/component.ejs'),
   page: path.join(templateBaseDir, 'alipay/page.ejs'),
   jsHelper: path.join(templateBaseDir, 'helper.js'),
