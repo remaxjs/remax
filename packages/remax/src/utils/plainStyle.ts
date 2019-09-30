56fd37956e3850a15cf7437afcf530d750151995
caihuanyu <eterlf41@gmail.com>
2019-09-30 19:32:48 +0800

    fix: 修复内联样式 vendor prefix 解析错误的问题
    fix #259

--- packages/remax/src/utils/plainStyle.ts
+++ packages/remax/src/utils/plainStyle.ts
@@ -1,11 +1,21 @@
 import { CSSProperties } from 'react';
 
-const transformReactStyleKey = (key: string) =>
-  key
-    .replace(/\.?([A-Z]+)/g, function(_x: any, y: string) {
-      return '-' + y.toLowerCase();
-    })
-    .replace(/^-/, '');
+const vendorPrefixes = ['webkit', 'moz', 'ms', 'o'];
+
+const transformReactStyleKey = (key: string) => {
+  let styleValue = key.replace(/\.?([A-Z]+)/g, function(_x: any, y: string) {
+    return '-' + y.toLowerCase();
+  });
+
+  const firstWord = styleValue.split('-').filter(s => s)[0];
+  styleValue = styleValue.replace(/^-/, '');
+
+  if (vendorPrefixes.find(prefix => prefix === firstWord)) {
+    styleValue = '-' + styleValue;
+  }
+
+  return styleValue;
+};
 
 const plainStyle = (style: CSSProperties | null | undefined) => {
   if (!style) {
