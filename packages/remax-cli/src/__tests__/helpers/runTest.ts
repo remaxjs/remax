3bc0f71ec84cc9ace8d4dab2a2abba9e7f26aca7
caihuanyu <eterlf41@gmail.com>
2019-10-08 10:59:40 +0800

    fix: 修复 rebuild 时不存在的属性依然残留的问题
    fix #266

--- packages/remax-cli/src/__tests__/helpers/runTest.ts
+++ packages/remax-cli/src/__tests__/helpers/runTest.ts
@@ -1,6 +1,5 @@
 import * as path from 'path';
 import build, { JEST_BUILD_TIMEOUT } from './build';
-import { clear as clearComponents } from '../../build/plugins/components';
 
 export default function runTest(app: string, target = 'alipay') {
   it(
@@ -8,7 +7,6 @@ export default function runTest(app: string, target = 'alipay') {
     async () => {
       process.env['REMAX_APP_TEST_VAL'] = `${app} test`;
 
-      clearComponents();
       const result = await build(app, target);
       expect(result).toMatchOutput(
         path.resolve(__dirname, `../fixtures/${app}/expected`)
