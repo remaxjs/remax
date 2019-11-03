f69c73ca0d0353a6c891c1b2ffb909fd60d38bb1
caihuanyu <eterlf41@gmail.com>
2019-11-03 10:44:06 +0800

    fix: 修复 windows 下图片引入路径错误的问题
    fix #311

---
+++ packages/remax-cli/src/__tests__/fixtures/assets/src/pages/index.js
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import { View, Image } from 'remax/alipay';
+import cat from '../assets/images/cat.png'
+import './index.css';
+
+export default () => <View>
+  <Image src={cat} />
+</View>;
