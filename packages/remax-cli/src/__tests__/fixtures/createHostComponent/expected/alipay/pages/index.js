cd5ca28b482ba3ff4f0bab85fb880bb08dda4608
caihuanyu <eterlf41@gmail.com>
2019-11-21 20:38:23 +0800

    fix: 修复安装的 lib 中 createHostComponent 不生效的问题

--- packages/remax-cli/src/__tests__/fixtures/createHostComponent/expected/alipay/pages/index.js
+++ packages/remax-cli/src/__tests__/fixtures/createHostComponent/expected/alipay/pages/index.js
@@ -2,12 +2,16 @@ import { createElement, Fragment } from 'react';
 import createHostComponent from '../npm/remax/esm/createHostComponent.js';
 import createPageConfig from '../npm/remax/esm/createPageConfig.js';
 import '../npm/remax/esm/index.js';
+import CustomComponent from '../npm/custom-component/index.js';
+
+var FooBar = createHostComponent('foo-bar');
 
 var _page = function _page() {
-  var FooBar = createHostComponent('foo-bar');
   return createElement(Fragment, null, createElement(FooBar, {
     foo: "bar",
     className: "class"
+  }), createElement(CustomComponent, {
+    foo: "bar"
   }));
 };
 
--- packages/remax-cli/src/__tests__/fixtures/createHostComponent/expected/toutiao/pages/index.js
+++ packages/remax-cli/src/__tests__/fixtures/createHostComponent/expected/toutiao/pages/index.js
@@ -6,12 +6,16 @@ var React = require('react');
 var createHostComponent = require('../npm/remax/esm/createHostComponent.js');
 var createPageConfig = require('../npm/remax/esm/createPageConfig.js');
 require('../npm/remax/esm/index.js');
+var index$2 = require('../npm/custom-component/index.js');
+
+var FooBar = createHostComponent.default('foo-bar');
 
 var _page = function _page() {
-  var FooBar = createHostComponent.default('foo-bar');
   return React.createElement(React.Fragment, null, React.createElement(FooBar, {
     foo: "bar",
     className: "class"
+  }), React.createElement(index$2.default, {
+    foo: "bar"
   }));
 };
 
