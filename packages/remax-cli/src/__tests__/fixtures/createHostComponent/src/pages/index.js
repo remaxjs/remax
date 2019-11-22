b6292a17b9c9e9daf679044c95af7af3b99744ec
Cai Huanyu <eterlf41@gmail.com>
2019-11-22 11:00:03 +0800

    fix: 修复 npm 包中 createHostComponent 不生效的问题 (#393)

--- packages/remax-cli/src/__tests__/fixtures/createHostComponent/src/pages/index.js
+++ packages/remax-cli/src/__tests__/fixtures/createHostComponent/src/pages/index.js
@@ -1,15 +1,14 @@
 import * as React from 'react';
+import CustomComponent from 'custom-component';
 import { createHostComponent } from 'remax/macro';
 
-export default () => {
-  const FooBar = createHostComponent('foo-bar', [
-    'foo',
-    ['class', 'className'],
-  ]);
+const FooBar = createHostComponent('foo-bar', ['foo', ['class', 'className']]);
 
+export default () => {
   return (
     <>
       <FooBar foo="bar" className="class" />
+      <CustomComponent foo="bar" />
     </>
   );
 };
