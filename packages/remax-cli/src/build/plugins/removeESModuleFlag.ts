e1a31f50df85c65434c6df9d1027280f1453b657
力皓 <lihao.ylh@alibaba-inc.com>
2019-11-28 10:47:55 +0800

    fix: support AssignmentExpression-style esmodule removal

--- packages/remax-cli/src/build/plugins/removeESModuleFlag.ts
+++ packages/remax-cli/src/build/plugins/removeESModuleFlag.ts
@@ -2,7 +2,35 @@ import { Plugin } from 'rollup';
 import { simple } from 'acorn-walk';
 import MagicString from 'magic-string';
 
-// __esModule 标示会影响  webpack 编译出来的模块
+const removeDefineProperty = (magicString: MagicString) => (node: any) => {
+  if (
+    node.callee.type === 'MemberExpression' &&
+    node.callee.object.name === 'Object' &&
+    node.callee.property.name === 'defineProperty' &&
+    node.arguments.length === 3 &&
+    node.arguments[0].name === 'exports' &&
+    node.arguments[1].value === '__esModule'
+  ) {
+    magicString.remove(node.start, node.end);
+  }
+};
+
+const removeExportsAssignment = (magicString: MagicString) => (node: any) => {
+  if (
+    node.operator === '=' &&
+    node.left.type === 'MemberExpression' &&
+    node.left.object.name === 'exports' &&
+    node.left.property.name === '__esModule' &&
+    node.right.value === true
+  ) {
+    magicString.remove(node.start, node.end);
+  }
+};
+
+// __esModule 标示会影响  webpack 编译出来的模块，需要删除
+// 两种情况：
+//  1. Object.defineProperty(exports, '__esModule', { value: true });
+//  2. exports.__esModule = true;
 export default function removeESModuleFlag(): Plugin {
   return {
     name: 'remove-esmodule-flag',
@@ -12,21 +40,9 @@ export default function removeESModuleFlag(): Plugin {
         sourceType: 'module',
       });
 
-      const remove = (node: any) => {
-        if (
-          node.callee.type === 'MemberExpression' &&
-          node.callee.object.name === 'Object' &&
-          node.callee.property.name === 'defineProperty' &&
-          node.arguments.length === 3 &&
-          node.arguments[0].name === 'exports' &&
-          node.arguments[1].value === '__esModule'
-        ) {
-          magicString.remove(node.start, node.end);
-        }
-      };
-
       simple(ast, {
-        CallExpression: remove,
+        CallExpression: removeDefineProperty(magicString),
+        AssignmentExpression: removeExportsAssignment(magicString),
       });
 
       return {
