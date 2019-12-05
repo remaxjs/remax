3f6c8432129dd635900789a0cffe69a831c73a80
Cai Huanyu <eterlf41@gmail.com>
2019-12-05 11:05:18 +0800

    build: bump babel-plugin-tester (#453)

--- packages/remax.macro/tests/fixtures/createHostComponent/multiple/output.js
+++ packages/remax.macro/tests/fixtures/createHostComponent/multiple/output.js
@@ -1,4 +1,4 @@
-import { createHostComponent } from "remax";
+import { createHostComponent } from 'remax';
 createHostComponent('host-component-a');
 createHostComponent('host-component-b');
 createHostComponent('host-component-c');
