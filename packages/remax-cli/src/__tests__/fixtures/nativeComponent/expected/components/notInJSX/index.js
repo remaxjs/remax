25137455007975b5c4562b9b0125e47e3e660157
Caihuanyu <eterlf41@gmail.com>
2019-12-18 09:47:07 +0800

    fix: 修复当原生组件带有导出内容时编译报错的问题

--- packages/remax-cli/src/__tests__/fixtures/nativeComponent/expected/components/notInJSX/index.js
+++ packages/remax-cli/src/__tests__/fixtures/nativeComponent/expected/components/notInJSX/index.js
@@ -2,6 +2,7 @@
 
 Object.defineProperty(exports, '__esModule', { value: true });
 
-Component({});var NotInJSXComponent = {};
+Component({});
+var NotInJSXComponent = {};
 
 exports.default = NotInJSXComponent;
