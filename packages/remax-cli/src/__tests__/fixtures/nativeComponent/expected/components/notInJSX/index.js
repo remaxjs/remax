62c738c233eb23ac25bfd735beedec9a2fcb184e
Caihuanyu <eterlf41@gmail.com>
2019-12-19 10:01:00 +0800

    Revert "fix: 修复当原生组件带有导出内容时编译报错的问题"
    This reverts commit c0f88c2a988c11bdc5a7eb350b583c205f1c01b2.

--- packages/remax-cli/src/__tests__/fixtures/nativeComponent/expected/components/notInJSX/index.js
+++ packages/remax-cli/src/__tests__/fixtures/nativeComponent/expected/components/notInJSX/index.js
@@ -2,7 +2,6 @@
 
 Object.defineProperty(exports, '__esModule', { value: true });
 
-Component({});
-var NotInJSXComponent = {};
+Component({});var NotInJSXComponent = {};
 
 exports.default = NotInJSXComponent;
