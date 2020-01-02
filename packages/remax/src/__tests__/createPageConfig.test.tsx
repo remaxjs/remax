991d00602241e01816d346bda0547b4d2a40885c
coppy <zheqingli@icloud.com>
2020-01-02 10:32:40 +0800

    test: refact test

--- packages/remax/src/__tests__/createPageConfig.test.tsx
+++ packages/remax/src/__tests__/createPageConfig.test.tsx
@@ -2,11 +2,19 @@ import * as React from 'react';
 import { createPageConfig, createAppConfig } from '..';
 import { usePageQuery } from '../hooks';
 
-// mock mini program getApp api
-const app = createAppConfig(undefined);
-// eslint-disable-next-line @typescript-eslint/ban-ts-ignore
-// @ts-ignore
-global.getApp = () => app;
+beforeAll(() => {
+  // mock mini program getApp api
+  const app = createAppConfig(undefined);
+  // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+  // @ts-ignore
+  global.getApp = () => app;
+});
+
+afterAll(() => {
+  // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+  // @ts-ignore
+  global.getApp = undefined;
+});
 
 function Page<T>(config: T) {
   return Object.assign(config, {
@@ -17,17 +25,16 @@ function Page<T>(config: T) {
 
 describe('page query hook', () => {
   it('usePageQuery will get the query from onLoad', () => {
-    const fn = jest.fn();
+    const usePageQueryMock = jest.fn(usePageQuery);
     const pageQuery = { id: 1 };
     const TestPage = () => {
-      const query = usePageQuery<typeof pageQuery>();
-      fn();
+      const query = usePageQueryMock();
       expect(query).toBe(pageQuery);
       return <div>{query.id}</div>;
     };
     const page = Page(createPageConfig(TestPage));
     page.onLoad(pageQuery);
-    expect(fn).toBeCalled();
+    expect(usePageQueryMock).toBeCalled();
     page.onUnload();
   });
 });
