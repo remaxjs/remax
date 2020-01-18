5f6ccfd8a434d85872ce8a93b796ce3e3f894874
Wei Zhu <yesmeck@gmail.com>
2020-01-18 13:25:56 +0800

    chore: update snapshot

--- packages/remax-cli/src/__tests__/config.test.ts
+++ packages/remax-cli/src/__tests__/config.test.ts
@@ -16,7 +16,7 @@ describe('remax config', () => {
     expect(() => {
       getConfig();
     }).toThrowErrorMatchingInlineSnapshot(`
-"Invalid configuration object. remax has been initialised using a configuration object that does not match the API schema.
+"Invalid configuration object. remax has been initialized using a configuration object that does not match the API schema.
  - configuration has an unknown property 'xxx'. These properties are valid:
    object { cssModules?, pxToRpx?, cwd?, progress?, compressTemplate?, output?, rootDir?, UNSAFE_wechatTemplateDepth?, alias?, postcss?, rollupOptions? }
  - configuration.cssModules should be one of these:
