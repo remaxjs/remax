3e8151e29c983e6c4ce6c9d479c171afc269b21e
Iron Lu <IronLu233@users.noreply.github.com>
2020-02-10 09:55:57 +0800

    fix: 字节跳动修正 PickerView 的 indicatorStyle 属性类型定义 (#633)

--- packages/remax-toutiao/src/hostComponents/PickerView/index.ts
+++ packages/remax-toutiao/src/hostComponents/PickerView/index.ts
@@ -1,10 +1,12 @@
+import * as React from 'react';
+
 import createHostComponent from '../../createHostComponent';
 
 import { BaseProps } from '../../types/component';
 
 export interface PickerViewProps extends BaseProps {
   value?: number[];
-  indicatorStyle: string;
+  indicatorStyle?: React.CSSProperties;
   maskStyle?: string;
   onChange?: (e: any) => void;
 }
