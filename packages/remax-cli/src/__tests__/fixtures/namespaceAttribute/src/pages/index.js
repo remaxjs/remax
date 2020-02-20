4ec8e6544de4e0cc0e8f88c3e6f1556a55da0391
Caihuanyu <eterlf41@gmail.com>
2020-02-20 15:06:04 +0800

    fix: 原生组件支持命名空间属性

--- packages/remax-cli/src/__tests__/fixtures/namespaceAttribute/src/pages/index.js
+++ packages/remax-cli/src/__tests__/fixtures/namespaceAttribute/src/pages/index.js
@@ -1,7 +1,13 @@
 import * as React from 'react';
 import { View } from 'remax/alipay';
+import NativeComponent from '@/components/native-component';
 import './index.css';
 
 export default function Index() {
-  return <View ns:prop="ns:value" />;
+  return (
+    <>
+      <View ns:prop="ns:value" />
+      <NativeComponent ns:prop="ns:value" />
+    </>
+  );
 }
