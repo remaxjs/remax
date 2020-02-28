686e73076e54b90c24b083189b3a80caa4aeedbe
皋天👿 <gaotian.wj@antfin.com>
2020-02-28 18:18:17 +0800

    chore: 格式化

--- packages/remax-cli/src/build/plugins/resolveCollect.ts
+++ packages/remax-cli/src/build/plugins/resolveCollect.ts
@@ -2,63 +2,62 @@ const path = require('path');
 const resolve = require('enhanced-resolve');
 
 export interface ResolveCollectionObject {
-    source: string;
-    importer: string;
-    isEntry: boolean;
-    rootEntry: ResolveCollectionObject | null;
+  source: string;
+  importer: string;
+  isEntry: boolean;
+  rootEntry: ResolveCollectionObject | null;
 }
 
 export const resolveList: ResolveCollectionObject[] = [];
 
 export function resolveCollection() {
-    return {
-        name: 'resolveCollection',
-        resolveId(source: string, importer: string) {
-            if (
-                source.indexOf('rollupPluginBabelHelpers.js') > -1 ||
-                source.indexOf('commonjsHelpers.js') > -1
-            ) {
-                return;
-            }
-            if (importer) {
-                source = resolve.sync(path.dirname(importer), source);
-            } else {
-                source = resolve.sync('', source);
-            }
+  return {
+    name: 'resolveCollection',
+    resolveId(source: string, importer: string) {
+      if (
+        source.indexOf('rollupPluginBabelHelpers.js') > -1 ||
+        source.indexOf('commonjsHelpers.js') > -1
+      ) {
+        return;
+      }
+      if (importer) {
+        source = resolve.sync(path.dirname(importer), source);
+      } else {
+        source = resolve.sync('', source);
+      }
 
-            if (!importer) {
-                resolveList.push({
-                    source,
-                    importer,
-                    isEntry: true,
-                    rootEntry: null,
-                });
-                return null;
-            }
+      if (!importer) {
+        resolveList.push({
+          source,
+          importer,
+          isEntry: true,
+          rootEntry: null,
+        });
+        return null;
+      }
 
-            const bundle = {
-                source,
-                importer,
-                isEntry: false,
-            };
+      const bundle = {
+        source,
+        importer,
+        isEntry: false,
+      };
 
-            resolveList.forEach(resolveId => {
-                if (resolveId.source === bundle.importer) {
-                    if (resolveId.isEntry) {
-                        resolveList.push({
-                            ...bundle,
-                            rootEntry: resolveId,
-                        });
-                    } else {
-                        resolveList.push({
-                            ...bundle,
-                            rootEntry: resolveId.rootEntry,
-                        });
-                    }
-                }
+      resolveList.forEach(resolveId => {
+        if (resolveId.source === bundle.importer) {
+          if (resolveId.isEntry) {
+            resolveList.push({
+              ...bundle,
+              rootEntry: resolveId,
             });
-            return null;
-        },
-    };
+          } else {
+            resolveList.push({
+              ...bundle,
+              rootEntry: resolveId.rootEntry,
+            });
+          }
+        }
+      });
+      return null;
+    },
+  };
 }
-
