8af45d6beae1b1757f9644a7f673857118ed1cf7
Caihuanyu <eterlf41@gmail.com>
2020-02-28 11:57:16 +0800

    add test

--- packages/remax-cli/src/__tests__/fixtures/alipay/expected/app.js
+++ packages/remax-cli/src/__tests__/fixtures/alipay/expected/app.js
@@ -6,14 +6,27 @@ var _rollupPluginBabelHelpers = require('./_virtual/_rollupPluginBabelHelpers.js
 var React = require('react');
 var createAppConfig = require('../../npm/remax/esm/createAppConfig.js');
 require('../../npm/remax/esm/index.js');
+var cat = require('./assets/images/cat.png.js');
 
 var _ref = /*#__PURE__*/function (_React$Component) {
   _rollupPluginBabelHelpers.inherits(_ref, _React$Component);
 
   function _ref() {
+    var _getPrototypeOf2;
+
+    var _this;
+
     _rollupPluginBabelHelpers.classCallCheck(this, _ref);
 
-    return _rollupPluginBabelHelpers.possibleConstructorReturn(this, _rollupPluginBabelHelpers.getPrototypeOf(_ref).apply(this, arguments));
+    for (var _len = arguments.length, args = new Array(_len), _key = 0; _key < _len; _key++) {
+      args[_key] = arguments[_key];
+    }
+
+    _this = _rollupPluginBabelHelpers.possibleConstructorReturn(this, (_getPrototypeOf2 = _rollupPluginBabelHelpers.getPrototypeOf(_ref)).call.apply(_getPrototypeOf2, [this].concat(args)));
+
+    _rollupPluginBabelHelpers.defineProperty(_rollupPluginBabelHelpers.assertThisInitialized(_this), "image", cat.default);
+
+    return _this;
   }
 
   _rollupPluginBabelHelpers.createClass(_ref, [{
