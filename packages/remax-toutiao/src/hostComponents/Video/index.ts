ee1a6f4626106a8fed657d5a1fa188df21cb692d
linjiajian999 <724159997@qq.com>
2020-03-10 22:04:27 +0800

    refactor: 优化头条 Video 组件属性 VideoProps
    参考文档：https://microapp.bytedance.com/dev/cn/mini-app/develop/component/media-component/video

--- packages/remax-toutiao/src/hostComponents/Video/index.ts
+++ packages/remax-toutiao/src/hostComponents/Video/index.ts
@@ -2,16 +2,40 @@ import createHostComponent from '../../createHostComponent';
 
 import { BaseProps } from '../../types/component';
 
+// docs: https://microapp.bytedance.com/dev/cn/mini-app/develop/component/media-component/video
 export interface VideoProps extends BaseProps {
-  src?: string;
+  /** 要播放视频的资源地址 1.0.0 */
+  src: string;
+  /** 是否自动播放 1.0.0 */
   autoplay?: boolean;
+  /** 视频封面的图片网络资源地址 1.0.0 */
   poster?: string;
-  onPlay?: (e: any) => void;
-  onPause?: (e: any) => void;
-  onEnded?: (e: any) => void;
-  onError?: (e: any) => void;
-  onTimeUpdate?: (e: any) => void;
-  onFullscreenchange?: (e: any) => void;
+  /** 当开始播放时触发 play 事件 1.0.0 */
+  onPlay?: (event: any) => any;
+  /** 当暂停播放时触发 pause 事件 1.0.0 */
+  onPause?: (event: any) => any;
+  /** 当播放到末尾时触发 ended 事件 1.0.0 */
+  onEnded?: (event: any) => any;
+  /** 视频播放出错时触发 1.0.0 */
+  onError?: (event: any) => any;
+  /** 播放进度变化时触发，event.detail = { currentTime, duration } 1.18.0 */
+  onTimeUpdate?: (event: any) => any;
+  /** 视频进入和退出全屏时触发 1.18.0 */
+  onFullScreenChange?: (event: any) => any;
+  /** 是否循环播放 1.47.0 */
+  loop?: boolean;
+  /** 是否显示全屏按钮 1.47.0 */
+  showFullscreenBtn?: boolean;
+  /** 是否显示播放、暂停、重播按钮，不包括视频封面的播放按钮 1.47.0 */
+  showPlayBtn?: boolean;
+  /** 是否显示全部播放控件 1.47.0 */
+  controls?: boolean;
+  /** 当视频大小与 video 容器大小不一致时，视频的表现形式：contain（包含），fill（填充），cover（覆盖） 1.47.0 */
+  objectFit?: 'contain' | 'fill' | 'cover';
+  /** 播放按钮的位置：center（视频中间），bottom（控制条上） 1.47.0 */
+  playBtnPosition?: 'center' | 'bottom';
+  /** 视频出现缓冲时触发 1.47.0 */
+  onWaiting?: (event: any) => any;
 }
 
 export default createHostComponent<VideoProps>('video');
