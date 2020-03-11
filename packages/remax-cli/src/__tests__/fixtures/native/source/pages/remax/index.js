a114e46027ddefed3fa4c3220e1c4fc389cfecf8
Caihuanyu <eterlf41@gmail.com>
2020-03-11 17:51:46 +0800

    fix test

--- packages/remax-cli/src/__tests__/fixtures/native/source/pages/remax/index.js
+++ packages/remax-cli/src/__tests__/fixtures/native/source/pages/remax/index.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { View } from 'remax/alipay';
 import NativeComponent from '@/components/native-component';
 
-export default function RemaxPage(props: any) {
+export default function RemaxPage(props) {
   return (
     <View>
       <NativeComponent />
