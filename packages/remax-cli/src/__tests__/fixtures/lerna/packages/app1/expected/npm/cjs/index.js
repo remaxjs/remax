c74c3816440273a4b5fd15096a9a0609c1e4c4d9
Caihuanyu <eterlf41@gmail.com>
2020-03-19 17:27:04 +0800

    update test snapshot

--- packages/remax-cli/src/__tests__/fixtures/lerna/packages/app1/expected/npm/cjs/index.js
+++ packages/remax-cli/src/__tests__/fixtures/lerna/packages/app1/expected/npm/cjs/index.js
@@ -2,6 +2,7 @@
 
 var _fmtEvent = _interopRequireDefault(require('./fmtEvent'));
 var moduleA= require('../module-a/index.js');
+import '../module-a/index.js';
 
 function _interopRequireDefault(obj) {
   return obj && obj.__esModule ? obj : { default: obj };
