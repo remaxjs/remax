9754b8715839eaf2ac23998cf28cff6b478cb8a4
Caihuanyu <eterlf41@gmail.com>
2020-04-03 18:11:15 +0800

    fix winPath

--- packages/remax-cli/src/__tests__/helpers/build.ts
+++ packages/remax-cli/src/__tests__/helpers/build.ts
@@ -116,7 +116,7 @@ export default async function build(app: string, target: PlatformTarget, options
       const include = options.include || [];
       const includeRegExp = new RegExp(`(${include.join('|')})`);
       const excludeRegExp = new RegExp(`(${exclude.join('|')})`);
-      const outputDir = path.join(cwd, remaxOptions.output);
+      const outputDir = winPath(path.join(cwd, remaxOptions.output));
       const output = getFilesInDir(fs, outputDir + '/', outputDir).filter(
         c =>
           (include.length > 0 && includeRegExp.test(c.fileName)) ||
