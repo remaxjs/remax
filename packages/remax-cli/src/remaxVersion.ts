9e6eae0bfd1b4657b4b6f392aa27e8fe6619c6d2
Caihuanyu <eterlf41@gmail.com>
2020-04-17 15:03:41 +0800

    build: 更新依赖关系

--- packages/remax-cli/src/remaxVersion.ts
+++ packages/remax-cli/src/remaxVersion.ts
@@ -1,13 +1,3 @@
-import path from 'path';
-import fs from 'fs';
-import winPath from './winPath';
-
 export default function remaxVersion() {
-  const remaxPackagePath: string = winPath(path.join(process.cwd(), 'node_modules', 'remax', 'package.json'));
-  if (fs.existsSync(remaxPackagePath)) {
-    const remaxPkgConfig = require(remaxPackagePath);
-    return remaxPkgConfig.version;
-  }
-
-  return '';
+  return require('remax/package.json').version;
 }
