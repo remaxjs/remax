24cbb66eb999c8277bed2de82d39f73cdb3c96ea
dominicleo <dominicleo@mail.com>
2020-04-18 18:06:29 +0800

    fix(wechat): 移除 LivePlayer 重复属性 onFullscreenChange

--- packages/remax-wechat/src/hostComponents/LivePlayer/node.ts
+++ packages/remax-wechat/src/hostComponents/LivePlayer/node.ts
@@ -16,7 +16,6 @@ export const alias = {
   autoPauseIfOpenNative: 'auto-pause-if-open-native',
   onStateChange: 'bindstatechange',
   onFullscreenChange: 'bindfullscreenchange',
-  onFullScreenChange: 'bindfullscreenchange',
   onNetStatus: 'bindnetstatus',
   animation: 'animation',
   pictureInPictureMode: 'picture-in-picture-mode',
