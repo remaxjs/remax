09959ce8c10c6e273ecbf6641331f4e68eb0e508
Caihuanyu <eterlf41@gmail.com>
2020-04-21 11:37:18 +0800

    docs(web): 说明 page 选择器和 vh 单位的问题

--- packages/remax-cli/src/web/pageClass.ts
+++ packages/remax-cli/src/web/pageClass.ts
@@ -1,10 +1,14 @@
 import postcss from 'postcss';
+import output from '../build/utils/output';
 
 export default postcss.plugin('postcss-remax-page-plugin', opts => {
   opts = opts || {};
   return (root, result) => {
     root.walkRules(rule => {
       if (rule.selector === 'page') {
+        output.warn(
+          '如果要兼容 web 应用，请不要在样式中使用 page 选择器，具体请参考 https://remaxjs.org/guide/one/web#样式'
+        );
         rule.selector = '.remax-page';
       }
     });
