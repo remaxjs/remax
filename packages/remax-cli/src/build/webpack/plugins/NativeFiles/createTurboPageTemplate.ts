5190f99ef433cd3dfcf489025f0d53d530d0d862
Meck Zhu <yesmeck@gmail.com>
2020-05-09 18:06:01 +0800

    test: 修复 windows 平台错误

--- packages/remax-cli/src/build/webpack/plugins/NativeFiles/createTurboPageTemplate.ts
+++ packages/remax-cli/src/build/webpack/plugins/NativeFiles/createTurboPageTemplate.ts
@@ -28,7 +28,9 @@ export default async function createTurboPageTemplate(
     return modules.includes(t.module);
   });
 
-  const entries = templateInfoMap.values().filter((template: any) => template.isEntry && template.module === pageFile);
+  const entries = templateInfoMap
+    .values()
+    .filter((template: any) => template.isEntry && winPath(template.module) === pageFile);
 
   renderOptions.components = (renderOptions.components as ComponentManifest[]).filter(c => {
     if (c.type !== 'native') {
