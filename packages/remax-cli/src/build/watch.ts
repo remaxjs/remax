7af0c794481c134acf962850d9eb374c9c8337d7
Meck Zhu <yesmeck@gmail.com>
2020-05-14 15:58:45 +0800

    fix: 修正配置文件的 watch

--- packages/remax-cli/src/build/watch.ts
+++ packages/remax-cli/src/build/watch.ts
@@ -4,14 +4,13 @@ import { Compiler } from 'webpack';
 import SingleEntryPlugin from 'webpack/lib/SingleEntryPlugin';
 import { getPages } from '../getEntries';
 
-let watchCounter = 0;
+let isRunning = true;
 
 export default function watch(options: Options, compiler: Compiler, watcher: any, addEntry = false) {
-  watchCounter += 1;
   // 监听额外的文件
   const pages = getPages(options);
   chokidar.watch([`${options.rootDir}/app.config.{js,ts}`]).on('change', () => {
-    if (watchCounter <= 1) return;
+    if (isRunning) return;
     if (addEntry) {
       const nextPages = getPages(options);
       nextPages.forEach(np => {
@@ -23,9 +22,17 @@ export default function watch(options: Options, compiler: Compiler, watcher: any
     watcher.invalidate();
   });
 
-  chokidar.watch([`${options.rootDir}/**/*.config.{js,ts}`]).on('all', () => {
-    if (watchCounter <= 1) return;
+  chokidar.watch([`${options.rootDir}/**/!(app).config.{js,ts}`]).on('all', () => {
+    if (isRunning) return;
     watcher.invalidate();
+  });
+
+  compiler.hooks.watchRun.tap('watchRun', () => {
+    isRunning = true;
+  });
+
+  compiler.hooks.done.tap('done', () => {
+    isRunning = false;
   });
 
   return watcher;
