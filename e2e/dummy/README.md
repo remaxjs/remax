35349ac6fe624d752e3512668e5f5bee1800d4d9
Meck Zhu <yesmeck@gmail.com>
2020-05-17 02:38:51 +0800

    test: 为 web 构建添加 e2e 测试

---
+++ e2e/dummy/README.md
@@ -0,0 +1,33 @@
+# Remax One
+
+使用 Remax 开发跨平台小程序。
+
+## Getting Start
+
+安装依赖
+
+```bash
+npm install
+```
+
+调试项目
+
+```bash
+# 选定要进行开发的平台，如 wechat，并调试
+$ npm run dev wechat
+or
+yarn dev wechat
+```
+
+使用小程序开发者工具打开项目下的 `dist/[target]` 目录。
+
+## 构建
+
+```bash
+# 选定要构建的平台，如 wechat，并执行构建
+$ npm run build wechat
+or
+$ yarn build wechat
+```
+
+使用小程序开发者工具打开项目下的 `dist/[target]` 目录，上传代码即可。
---
+++ e2e/dummy/tests/index.test.js
@@ -0,0 +1,20 @@
+import 'expect-puppeteer';
+
+function goTo(path) {
+  return page.goto('http://localhost:3000#' + path);
+}
+
+describe('Google', () => {
+  beforeAll(async () => {
+    await goTo('/');
+  });
+
+  it('renders page one', async () => {
+    await expect(page).toMatch('page one');
+  });
+
+  it('renders page two', async () => {
+    await goTo('/pages/two/index');
+    await expect(page).toMatch('page two');
+  });
+});
