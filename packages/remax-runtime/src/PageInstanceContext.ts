aa346c3fc5271fdbdebda14c5d679bd6d8508202
Meck Zhu <yesmeck@gmail.com>
2020-05-27 02:16:56 +0800

    refactor: 优化内部组件的 displayName

--- packages/remax-runtime/src/PageInstanceContext.ts
+++ packages/remax-runtime/src/PageInstanceContext.ts
@@ -2,4 +2,6 @@ import * as React from 'react';
 
 const PageInstanceContext = React.createContext<any>(null);
 
+PageInstanceContext.displayName = 'PageContext';
+
 export default PageInstanceContext;
