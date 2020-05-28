36e29217c2e5342a353a2d867954db63ad691f81
大宝 <noyobo@gmail.com>
2020-05-28 16:59:05 +0800

    style: prettier code

--- packages/remax-cli/src/build/watch.ts
+++ packages/remax-cli/src/build/watch.ts
@@ -9,27 +9,31 @@ let isRunning = true;
 export default function watch(options: Options, compiler: Compiler, watcher: any, addEntry = false) {
   // 监听额外的文件
   const pages = getPages(options);
-  chokidar.watch([`${options.rootDir}/app.config.{js,ts}`], {
-    cwd: options.cwd,
-  }).on('change', () => {
-    if (isRunning) return;
-    if (addEntry) {
-      const nextPages = getPages(options);
-      nextPages.forEach(np => {
-        if (!pages.find(p => p.filename === np.filename)) {
-          new SingleEntryPlugin(null, np.filename, np.name).apply(compiler);
-        }
-      });
-    }
-    watcher.invalidate();
-  });
+  chokidar
+    .watch([`${options.rootDir}/app.config.{js,ts}`], {
+      cwd: options.cwd,
+    })
+    .on('change', () => {
+      if (isRunning) return;
+      if (addEntry) {
+        const nextPages = getPages(options);
+        nextPages.forEach(np => {
+          if (!pages.find(p => p.filename === np.filename)) {
+            new SingleEntryPlugin(null, np.filename, np.name).apply(compiler);
+          }
+        });
+      }
+      watcher.invalidate();
+    });
 
-  chokidar.watch([`${options.rootDir}/**/!(app).config.{js,ts}`], {
-    cwd: options.cwd,
-  }).on('all', () => {
-    if (isRunning) return;
-    watcher.invalidate();
-  });
+  chokidar
+    .watch([`${options.rootDir}/**/!(app).config.{js,ts}`], {
+      cwd: options.cwd,
+    })
+    .on('all', () => {
+      if (isRunning) return;
+      watcher.invalidate();
+    });
 
   compiler.hooks.watchRun.tap('watchRun', () => {
     isRunning = true;
