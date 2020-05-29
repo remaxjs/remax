63021e0c215f3f75a71a1f419480b4c1c8612961
noyobo <noyobo@gmail.com>
2020-05-29 17:34:45 +0800

    feat(wechat): slider defaultProps

--- packages/remax-wechat/src/hostComponents/Slider/index.ts
+++ packages/remax-wechat/src/hostComponents/Slider/index.ts
@@ -2,12 +2,24 @@ import * as React from 'react';
 import { createHostComponent } from '@remax/shared';
 import { BaseProps } from '../../types/component';
 
+/**
+ * https://developers.weixin.qq.com/miniprogram/dev/component/slider.html
+ */
 export const Slider: React.ComponentType<SliderProps> = createHostComponent<SliderProps>('slider');
 
 Slider.defaultProps = {
-  blockSize: 28,
-  step: 1,
+  min: 0,
   max: 100,
+  step: 1,
+  disabled: false,
+  value: 0,
+  color: '#e9e9e9',
+  selectedColor: '#1aad19',
+  activeColor: '#1aad19',
+  backgroundColor: '#e9e9e9',
+  blockSize: 28,
+  blockColor: '#ffffff',
+  showValue: false,
 };
 
 export interface SliderProps extends BaseProps {
@@ -22,6 +34,10 @@ export interface SliderProps extends BaseProps {
   disabled?: boolean;
   /** (default: 0) 当前取值 1.0.0 */
   value?: number;
+  /** 背景条的颜色（请使用 backgroundColor） 1.0.0 */
+  color?: string;
+  /** 已选择的颜色（请使用 activeColor） 1.0.0 */
+  selectedColor?: string;
   /** (default: #1aad19) 已选择的颜色 1.0.0 */
   activeColor?: string;
   /** (default: #e9e9e9) 背景条的颜色 1.0.0 */
