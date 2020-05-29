6cb1cbaf0364d848367736bf65192b3d09c6aa8d
noyobo <noyobo@gmail.com>
2020-05-29 17:34:08 +0800

    feat(wechat): open-data defaultProps

--- packages/remax-wechat/src/hostComponents/OpenData/index.ts
+++ packages/remax-wechat/src/hostComponents/OpenData/index.ts
@@ -23,5 +23,11 @@ export interface OpenDataProps extends BaseProps {
   /** 群名称或用户信息为空时触发 2.8.1 */
   onError?: (event: any) => any;
 }
-
+/**
+ * https://developers.weixin.qq.com/miniprogram/dev/component/open-data.html
+ */
 export const OpenData = createHostComponent<OpenDataProps>('open-data');
+
+OpenData.defaultProps = {
+  lang: 'en',
+};
