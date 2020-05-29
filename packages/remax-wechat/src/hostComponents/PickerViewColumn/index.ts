2451ad5db90ca7f859b8be02889e06f9c8dc24c5
noyobo <noyobo@gmail.com>
2020-05-29 17:34:20 +0800

    feat(wechat): picker-view-column defaultProps

--- packages/remax-wechat/src/hostComponents/PickerViewColumn/index.ts
+++ packages/remax-wechat/src/hostComponents/PickerViewColumn/index.ts
@@ -3,4 +3,7 @@ import { BaseProps } from '../../types/component';
 
 export type PickerViewColumnProps = BaseProps;
 
+/**
+ * https://developers.weixin.qq.com/miniprogram/dev/component/picker-view-column.html
+ */
 export const PickerViewColumn = createHostComponent<PickerViewColumnProps>('picker-view-column');
