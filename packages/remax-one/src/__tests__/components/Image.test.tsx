3c7397ffdf756dfc9608ed20d5c46e1bcdfe0acf
Caihuanyu <eterlf41@gmail.com>
2020-06-04 20:12:16 +0800

    test: update test snapshot

--- packages/remax-one/src/__tests__/components/Image.test.tsx
+++ packages/remax-one/src/__tests__/components/Image.test.tsx
@@ -3,7 +3,42 @@ import TestRenderer from 'react-test-renderer';
 import { Image } from '../../hostComponents';
 
 describe('Image', () => {
-  it('render correctly', () => {
+  it('render correctly in ali', () => {
+    process.env.REMAX_PLATFORM = 'ali';
+    const testRenderer = TestRenderer.create(
+      <Image
+        className="class"
+        onError={() => {
+          // ignore
+        }}
+        onLoad={() => {
+          // ignore
+        }}
+      />
+    );
+
+    expect(testRenderer.toJSON()).toMatchSnapshot();
+  });
+
+  it('render correctly in wechat', () => {
+    process.env.REMAX_PLATFORM = 'wechat';
+    const testRenderer = TestRenderer.create(
+      <Image
+        className="class"
+        onError={() => {
+          // ignore
+        }}
+        onLoad={() => {
+          // ignore
+        }}
+      />
+    );
+
+    expect(testRenderer.toJSON()).toMatchSnapshot();
+  });
+
+  it('render correctly in toutiao', () => {
+    process.env.REMAX_PLATFORM = 'toutiao';
     const testRenderer = TestRenderer.create(
       <Image
         className="class"
