b25d8cee5de2474d7480e48ef4d89dd99575f07f
Caihuanyu <eterlf41@gmail.com>
2020-06-05 13:48:18 +0800

    test: update snapshot

--- packages/remax-cli/src/__tests__/integration/fixtures/nativeComponent/expected/ali/pages/index2.js
+++ packages/remax-cli/src/__tests__/integration/fixtures/nativeComponent/expected/ali/pages/index2.js
@@ -111,7 +111,7 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _remax_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(2);
 /* harmony import */ var _remax_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_remax_runtime__WEBPACK_IMPORTED_MODULE_0__);
 
-/* harmony default export */ __webpack_exports__["default"] = (Object(_remax_runtime__WEBPACK_IMPORTED_MODULE_0__["createNativeComponent"])('index9'));
+/* harmony default export */ __webpack_exports__["default"] = (Object(_remax_runtime__WEBPACK_IMPORTED_MODULE_0__["createNativeComponent"])('index10'));
 
 /***/ })
 ],[[18,0,7]]]);
