a204ec7dd3ade1b6b97cd834f018fafa67ec960a
Wei Zhu <yesmeck@gmail.com>
2020-06-18 19:48:35 +0800

    fix(wechat): 修复条件渲染错误 (#1100)
    close #1096
    close #1099
    
    Co-authored-by: Caihuanyu <eterlf41@gmail.com>

--- packages/remax-runtime/src/__tests__/index.wechat.test.tsx
+++ packages/remax-runtime/src/__tests__/index.wechat.test.tsx
@@ -7,6 +7,17 @@ import { reset as resetInstanceId } from '../instanceId';
 import Container from '../Container';
 import { useNativeEffect } from '../hooks';
 
+function delay(ms: number) {
+  if (typeof ms !== 'number') {
+    throw new Error('Must specify ms');
+  }
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve();
+    }, ms);
+  });
+}
+
 const p = {
   setData(state: any, callback: Function) {
     setTimeout(() => {
@@ -192,211 +203,334 @@ describe('wechat remax render', () => {
 
     expect(actions).toMatchSnapshot();
   });
-});
 
-it('create proxy for onClick callback', () => {
-  const view = React.createRef<any>();
-  const handleClick = () => void 0;
-  const handleAnimationStart = () => void 0;
-  class Page extends React.Component {
-    render() {
-      return <View ref={view} onClick={handleClick} onAnimationStart={handleAnimationStart} />;
-    }
-  }
-  const container = new Container(p);
-  render(<Page />, container);
+  it('remove and add children', async () => {
+    const logs: any = [];
+    class App extends React.Component {
+      state = {
+        toggle: false,
+      };
 
-  function findFn(name: string) {
-    const fnKeys = Object.keys(view.current.container.context);
-    const fnKey = fnKeys.find(key => key.indexOf(name) !== -1) || '';
-    return view.current.container.context[fnKey];
-  }
-
-  const newHandleClick = findFn('onTap');
-  const newHandleAnimationStart = findFn('onAnimationStart');
-
-  expect(newHandleClick).not.toBe(handleClick);
-  expect(newHandleAnimationStart).toBe(handleAnimationStart);
-});
-
-it('useEffect works', done => {
-  const Page = () => {
-    React.useEffect(() => {
-      done();
-    });
-
-    return <View>app</View>;
-  };
-  const container = new Container(p);
-  render(<Page />, container);
-});
-
-it('pure rerender when props changed', done => {
-  const payload: any[] = [];
-  const context = {
-    setData: (data: any) => {
-      payload.push(data);
-    },
-  };
-
-  class Page extends React.Component {
-    state = {
-      value: 'foo',
-    };
-
-    setValue(value: string) {
-      this.setState({ value });
-    }
-
-    render() {
-      return (
-        <View style={{ width: '32px' }}>
-          <Input value={this.state.value} />
-        </View>
-      );
-    }
-  }
-  const container = new Container(context);
-  const page = React.createRef<any>();
-  render(<Page ref={page} />, container);
-
-  expect.assertions(2);
-
-  page.current.setValue('bar');
-
-  setTimeout(() => {
-    expect(payload).toHaveLength(2);
-    expect(payload[1]).toMatchInlineSnapshot(`
-      Object {
-        "root.nodes.7.nodes.6.props.value": "bar",
+      toggle() {
+        this.setState({
+          toggle: true,
+        });
       }
-    `);
-    done();
-  }, 5);
-});
 
-it('pure rerender when props delete', done => {
-  const payload: any[] = [];
-  const context = {
-    setData: (data: any) => {
-      payload.push(data);
-    },
-  };
+      render() {
+        const { toggle } = this.state;
+        return (
+          <>
+            <View id="1">
+              <View id="2">{toggle ? <View id="3">a</View> : <View id="3">b</View>}</View>
+              {!toggle && <View id="5">safeAreaHeight</View>}
+            </View>
+          </>
+        );
+      }
+    }
 
-  class Page extends React.Component {
-    state = {
-      value: 'foo',
+    const container = new Container({
+      setData(action: any) {
+        logs.push(action);
+      },
+    });
+    const app = React.createRef<any>();
+    render(<App ref={app} />, container);
+    app.current.toggle();
+    await delay(100);
+    expect(logs.pop()).toEqual({
+      'root.nodes.6.children': [3],
+      'root.nodes.6.nodes.3.nodes.2.nodes.1': {
+        id: 1,
+        text: 'a',
+        type: 'plain-text',
+      },
+      'root.nodes.6.nodes.5': null,
+    });
+  });
+
+  it("does not update deleted node's children node", async () => {
+    const logs: any = [];
+    class App extends React.Component {
+      state = {
+        a: true,
+        b: true,
+      };
+
+      toggle() {
+        this.setState({
+          b: false,
+        });
+        this.setState({
+          a: false,
+        });
+      }
+
+      render() {
+        const { a, b } = this.state;
+        // eslint-disable-next-line prettier/prettier
+        return <View id="a">{a && <View id="b">{b ? <View id="c">a</View> : 'b'}</View>}</View>;
+      }
+    }
+
+    const container = new Container({
+      setData(action: any) {
+        logs.push(action);
+      },
+    });
+    const app = React.createRef<any>();
+    render(<App ref={app} />, container);
+    app.current.toggle();
+    await delay(100);
+    expect(logs.pop()).toEqual({
+      'root.nodes.4.children': [],
+      'root.nodes.4.nodes.3': null,
+    });
+  });
+
+  it('does not update deleted node', async () => {
+    const logs: any = [];
+    class App extends React.Component {
+      state = {
+        a: true,
+        b: true,
+      };
+
+      toggle() {
+        this.setState({
+          b: false,
+        });
+        this.setState({
+          a: false,
+        });
+      }
+
+      render() {
+        const { a, b } = this.state;
+        return <View>{a && <View style={{ color: b ? 'red' : 'green' }}>foo</View>}</View>;
+      }
+    }
+
+    const container = new Container({
+      setData(action: any) {
+        logs.push(action);
+      },
+    });
+    const app = React.createRef<any>();
+    render(<App ref={app} />, container);
+    app.current.toggle();
+    await delay(100);
+    expect(logs.pop()).toEqual({
+      'root.nodes.3.children': [],
+      'root.nodes.3.nodes.2': null,
+    });
+  });
+
+  it('create proxy for onClick callback', () => {
+    const view = React.createRef<any>();
+    const handleClick = () => void 0;
+    const handleAnimationStart = () => void 0;
+    class Page extends React.Component {
+      render() {
+        return <View ref={view} onClick={handleClick} onAnimationStart={handleAnimationStart} />;
+      }
+    }
+    const container = new Container(p);
+    render(<Page />, container);
+
+    function findFn(name: string) {
+      const fnKeys = Object.keys(view.current.container.context);
+      const fnKey = fnKeys.find(key => key.indexOf(name) !== -1) || '';
+      return view.current.container.context[fnKey];
+    }
+
+    const newHandleClick = findFn('onTap');
+    const newHandleAnimationStart = findFn('onAnimationStart');
+
+    expect(newHandleClick).not.toBe(handleClick);
+    expect(newHandleAnimationStart).toBe(handleAnimationStart);
+  });
+
+  it('useEffect works', done => {
+    const Page = () => {
+      React.useEffect(() => {
+        done();
+      });
+
+      return <View>app</View>;
+    };
+    const container = new Container(p);
+    render(<Page />, container);
+  });
+
+  it('pure rerender when props changed', done => {
+    const payload: any[] = [];
+    const context = {
+      setData: (data: any) => {
+        payload.push(data);
+      },
     };
 
-    setValue(value: string) {
-      this.setState({ value });
+    class Page extends React.Component {
+      state = {
+        value: 'foo',
+      };
+
+      setValue(value: string) {
+        this.setState({ value });
+      }
+
+      render() {
+        return (
+          <View style={{ width: '32px' }}>
+            <Input value={this.state.value} />
+          </View>
+        );
+      }
     }
+    const container = new Container(context);
+    const page = React.createRef<any>();
+    render(<Page ref={page} />, container);
 
-    render() {
-      return (
-        <View style={{ width: '32px' }}>{!this.state.value ? <Input /> : <Input value={this.state.value} />}</View>
-      );
-    }
-  }
-  const container = new Container(context);
-  const page = React.createRef<any>();
-  render(<Page ref={page} />, container);
+    expect.assertions(2);
 
-  expect.assertions(2);
+    page.current.setValue('bar');
 
-  page.current.setValue(undefined);
-
-  setTimeout(() => {
-    expect(payload).toHaveLength(2);
-    expect(payload).toMatchInlineSnapshot(`
-      Array [
+    setTimeout(() => {
+      expect(payload).toHaveLength(2);
+      expect(payload[1]).toMatchInlineSnapshot(`
         Object {
-          "root.children": Array [
-            10,
-          ],
-          "root.nodes.10": Object {
-            "children": Array [
-              9,
+          "root.nodes.2.nodes.1.props.value": "bar",
+        }
+      `);
+      done();
+    }, 5);
+  });
+
+  it('pure rerender when props delete', done => {
+    const payload: any[] = [];
+    const context = {
+      setData: (data: any) => {
+        payload.push(data);
+      },
+    };
+
+    class Page extends React.Component {
+      state = {
+        value: 'foo',
+      };
+
+      setValue(value: string) {
+        this.setState({ value });
+      }
+
+      render() {
+        return (
+          <View style={{ width: '32px' }}>{!this.state.value ? <Input /> : <Input value={this.state.value} />}</View>
+        );
+      }
+    }
+    const container = new Container(context);
+    const page = React.createRef<any>();
+    render(<Page ref={page} />, container);
+
+    expect.assertions(2);
+
+    page.current.setValue(undefined);
+
+    setTimeout(() => {
+      expect(payload).toHaveLength(2);
+      expect(payload).toMatchInlineSnapshot(`
+        Array [
+          Object {
+            "root.children": Array [
+              2,
             ],
-            "id": 10,
-            "nodes": Object {
-              "9": Object {
-                "children": Array [],
-                "id": 9,
-                "props": Object {
-                  "value": "foo",
+            "root.nodes.2": Object {
+              "children": Array [
+                1,
+              ],
+              "id": 2,
+              "nodes": Object {
+                "1": Object {
+                  "children": Array [],
+                  "id": 1,
+                  "props": Object {
+                    "value": "foo",
+                  },
+                  "text": undefined,
+                  "type": "input",
                 },
-                "text": undefined,
-                "type": "input",
               },
+              "props": Object {
+                "style": "width:32rpx;",
+              },
+              "text": undefined,
+              "type": "view",
             },
-            "props": Object {
-              "style": "width:32rpx;",
-            },
-            "text": undefined,
-            "type": "view",
           },
-        },
-        Object {
-          "root.nodes.10.nodes.9.props.value": null,
-        },
-      ]
-    `);
-    done();
-  }, 5);
-});
+          Object {
+            "root.nodes.2.nodes.1.props.value": null,
+          },
+        ]
+      `);
+      done();
+    }, 5);
+  });
 
-it('useNativeEffect once works', done => {
-  let count = 0;
-  const Page = () => {
-    const [width, setWidth] = React.useState(0);
-    useNativeEffect(() => {
-      count += 1;
+  it('useNativeEffect once works', done => {
+    let count = 0;
+    const Page = () => {
+      const [width, setWidth] = React.useState(0);
+      useNativeEffect(() => {
+        count += 1;
 
-      setTimeout(() => {
-        if (count === 1) {
+        setTimeout(() => {
+          if (count === 1) {
+            done();
+          }
+        }, 500);
+      }, []);
+      React.useEffect(() => {
+        setTimeout(() => {
+          setWidth(100);
+        }, 100);
+      }, []);
+
+      return <View>{width}</View>;
+    };
+    const container = new Container(p);
+    render(<Page />, container);
+  });
+
+  it('useNativeEffect deps works', done => {
+    let count = 0;
+    const Page = () => {
+      const [width, setWidth] = React.useState(0);
+      const [height, setheight] = React.useState(0);
+      useNativeEffect(() => {
+        count += 1;
+
+        if (count === 2) {
           done();
         }
-      }, 500);
-    }, []);
-    React.useEffect(() => {
-      setTimeout(() => {
-        setWidth(100);
-      }, 100);
-    }, []);
+      }, [width]);
+      React.useEffect(() => {
+        setheight(100);
+        setTimeout(() => {
+          setWidth(100);
+        }, 1000);
+      }, []);
 
-    return <View>{width}</View>;
-  };
-  const container = new Container(p);
-  render(<Page />, container);
-});
-
-it('useNativeEffect deps works', done => {
-  let count = 0;
-  const Page = () => {
-    const [width, setWidth] = React.useState(0);
-    const [height, setheight] = React.useState(0);
-    useNativeEffect(() => {
-      count += 1;
-
-      if (count === 2) {
-        done();
-      }
-    }, [width]);
-    React.useEffect(() => {
-      setheight(100);
-      setTimeout(() => {
-        setWidth(100);
-      }, 1000);
-    }, []);
-
-    return (
-      <View>
-        {width}
-        {height}
-      </View>
-    );
-  };
-  const container = new Container(p);
-  render(<Page />, container);
+      return (
+        <View>
+          {width}
+          {height}
+        </View>
+      );
+    };
+    const container = new Container(p);
+    render(<Page />, container);
+  });
 });
