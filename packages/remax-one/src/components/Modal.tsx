0b8a927e71a51ce0cb5546e64a982eace0481a27
Caihuanyu <eterlf41@gmail.com>
2020-07-06 10:35:32 +0800

    rename React to react

--- packages/remax-one/src/components/Modal.tsx
+++ packages/remax-one/src/components/Modal.tsx
@@ -1,4 +1,4 @@
-import * as React from 'React';
+import * as React from 'react';
 import { usePageContext, createPortal } from '@remax/runtime';
 
 export default function RemaxModal({ children }: React.PropsWithChildren<{}>) {
