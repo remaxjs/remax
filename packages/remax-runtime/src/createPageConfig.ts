4e8fef1dfe5f26824aecc0d530da23cf48aef758
Cai Huanyu <eterlf41@gmail.com>
2020-09-16 13:46:14 +0800

    fix: 修复同一页面的生命周期会重复触发的问题 (#1289)
    fix #1288

--- packages/remax-runtime/src/createPageConfig.ts
+++ packages/remax-runtime/src/createPageConfig.ts
@@ -20,33 +20,36 @@ export function resetPageId() {
   idCounter = 0;
 }
 
-const defaultData = {
-  root: {
-    children: [],
-  },
-  modalRoot: {
-    children: [],
-  },
-};
-
-const defaultLifecycleCallback: any = {};
-
 export default function createPageConfig(Page: React.ComponentType<any>, name: string) {
   const app = getApp() as any;
 
   const config: any = {
-    data: defaultData,
+    data: {
+      root: {
+        children: [],
+      },
+      modalRoot: {
+        children: [],
+      },
+    },
 
     wrapperRef: React.createRef<any>(),
 
-    lifecycleCallback: defaultLifecycleCallback,
+    lifecycleCallback: {},
 
     onLoad(this: any, query: any) {
       const PageWrapper = createPageWrapper(Page, name);
       this.pageId = generatePageId();
 
-      this.lifecycleCallback = defaultLifecycleCallback;
-      this.data = defaultData;
+      this.lifecycleCallback = {};
+      this.data = {
+        root: {
+          children: [],
+        },
+        modalRoot: {
+          children: [],
+        },
+      };
 
       this.query = query;
       this.container = new Container(this, 'root');
@@ -114,25 +117,25 @@ export default function createPageConfig(Page: React.ComponentType<any>, name: s
     events: {
       // 页面返回时触发
       onBack(this: any, e: any) {
-        return config.callLifecycle(Lifecycle.back, e);
+        return this.callLifecycle(Lifecycle.back, e);
       },
 
       // 键盘高度变化时触发
       onKeyboardHeight(this: any, e: any) {
-        return config.callLifecycle(Lifecycle.keyboardHeight, e);
+        return this.callLifecycle(Lifecycle.keyboardHeight, e);
       },
 
       onTabItemTap(this: any, e: any) {
-        return config.callLifecycle(Lifecycle.tabItemTap, e);
+        return this.callLifecycle(Lifecycle.tabItemTap, e);
       },
 
       // 点击但切换tabItem前触发
       beforeTabItemTap(this: any) {
-        return config.callLifecycle(Lifecycle.beforeTabItemTap);
+        return this.callLifecycle(Lifecycle.beforeTabItemTap);
       },
 
       onResize(this: any, e: any) {
-        return config.callLifecycle(Lifecycle.resize, e);
+        return this.callLifecycle(Lifecycle.resize, e);
       },
     },
 
