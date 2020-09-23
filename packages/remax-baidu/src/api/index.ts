13aa680f0ca6914a9d5dc9d5bb69e5aad4ef7c29
noyobo <noyobo@gmail.com>
2020-09-23 12:30:28 +0800

    chore: 增加 pageScrollTo

--- packages/remax-baidu/src/api/index.ts
+++ packages/remax-baidu/src/api/index.ts
@@ -1,4 +1,6 @@
+import { promisify } from '@remax/shared';
 export const getAppStub = getApp;
 
 export const canIUse = swan.canIUse;
 export const getSystemInfoSync = swan.getSystemInfoSync;
+export const pageScrollTo = promisify(swan.pageScrollTo);
