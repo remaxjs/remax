1bfcd222aa844ba0bac7247a9235bf6eebe5449a
iugo <iugogogo@gmail.com>
2020-11-03 09:17:41 +0800

    fix: 完善 useQuery 类型定义 (#1350)
    完善 useQuery 类型定义

--- packages/remax-runtime/src/hooks/useQuery.ts
+++ packages/remax-runtime/src/hooks/useQuery.ts
@@ -1,7 +1,9 @@
 import { useContext } from 'react';
 import PageContext from '../PageContext';
 
-export default function useQuery<Q extends Record<string, string> = { [name: string]: string }>(): Q {
+export default function useQuery<
+  Q extends Record<string, string | undefined> = { [name: string]: string | undefined }
+>(): Q {
   const pageInstance: any = useContext(PageContext)?.page;
   return pageInstance?.query;
 }
--- packages/remax-runtime/src/hooks/useQuery.web.ts
+++ packages/remax-runtime/src/hooks/useQuery.web.ts
@@ -1,6 +1,8 @@
 import { useLocation } from '@remax/web';
 import qs from 'qs';
 
-export default function useQuery<Q extends Record<string, string> = { [name: string]: string }>(): Q {
+export default function useQuery<
+  Q extends Record<string, string | undefined> = { [name: string]: string | undefined }
+>(): Q {
   return qs.parse(useLocation().search, { ignoreQueryPrefix: true }) as Q;
 }
