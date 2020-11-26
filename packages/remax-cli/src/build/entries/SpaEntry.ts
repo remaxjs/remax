93fc4d8ad450dbb1d71ae61906a9b5342646bade
juvham <juvham@foxmail.com>
2020-11-26 22:22:11 +0800

    fix: 修复缺少括号语法错误

--- packages/remax-cli/src/build/entries/SpaEntry.ts
+++ packages/remax-cli/src/build/entries/SpaEntry.ts
@@ -34,7 +34,7 @@ export default class SpaEntry extends VirtualEntry {
     /* webpackChunkName: "${entry.name}" */
     '${entry.filename}'
   )
-`
+}`
             )
             .join(',')}
         ],
