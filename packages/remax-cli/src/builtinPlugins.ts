647b10cfdec03de73d6791a7c37024c1c82a71ca
hehe <xpc1993@gmail.com>
2020-12-08 23:29:06 +0800

    chore: skip plugin-error-screen on production

--- packages/remax-cli/src/builtinPlugins.ts
+++ packages/remax-cli/src/builtinPlugins.ts
@@ -9,17 +9,22 @@ export const builtinPlugins = (
   init: (...args: any[]) => Plugin;
 }> => {
   const isDev = process.env.NODE_ENV !== 'production';
+  if (!isDev) {
+    return [];
+  }
+
   const plugins = [
     {
       optionKey: 'errorScreen',
       init: errorScreen,
     },
   ];
-  if (options.target !== Platform.web && isDev && options.devtools) {
+  if (options.target !== Platform.web && options.devtools) {
     plugins.push({
       optionKey: 'devtools',
       init: devtools,
     });
   }
+
   return plugins;
 };
