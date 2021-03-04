b319d9b2a568ba3d090a6fe8e52f761adcdb4506
何锦余 <cadenho@hotmail.com>
2021-03-04 13:27:14 +0800

    fix(wechat): 修复 Map 组件缺失的属性 (#1489)
    * fix(wechat): 修复 Map 组件缺失的属性
    
    * fix(wechat): 补充 Map 组件 defaultProps
    
    * feat(wechat): 更新 Snapshot
    
    Co-authored-by: hekaijun <hekaijun@xiaoheiban.cn>

--- packages/remax-wechat/src/hostComponents/Map/index.ts
+++ packages/remax-wechat/src/hostComponents/Map/index.ts
@@ -9,6 +9,10 @@ export interface MapProps extends BaseProps {
   latitude: number;
   /** (default: 16) 缩放级别，取值范围为3-20 1.0.0 */
   scale?: number;
+  /** (default: 3) 最小缩放级别 2.13.0 */
+  minScale?: number;
+  /** (default: 20) 最大缩放级别 2.13.0 */
+  maxScale?: number;
   /** 标记点 1.0.0 */
   markers?: any[];
   /** 即将移除，请使用 markers 1.0.0 */
@@ -51,6 +55,10 @@ export interface MapProps extends BaseProps {
   enableSatellite?: boolean;
   /** (default: false) 是否开启实时路况 2.7.0 */
   enableTraffic?: boolean;
+  /** (default: false) 是否展示 POI 点 2.14.0 */
+  enablePoi?: boolean;
+  /** (default: false) 是否展示建筑物 2.14.0 */
+  enableBuilding?: boolean;
   /** 配置项 2.8.2 */
   setting?: any;
   /** 点击标记点时触发，e.detail = {markerId} 1.0.0 */
@@ -65,6 +73,8 @@ export interface MapProps extends BaseProps {
   onRegionChange?: (event: GenericEvent) => any;
   /** 点击地图poi点时触发，e.detail = {name, longitude, latitude} 2.3.0 */
   onPoiTap?: (event: GenericEvent) => any;
+  /** 点击定位标时触发，e.detail = {longitude, latitude} 2.13.0 */
+  onAnchorPointTap?: (event: GenericEvent) => any;
 }
 /**
  * https://developers.weixin.qq.com/miniprogram/dev/component/map.html
@@ -73,6 +83,8 @@ export const Map: React.ComponentType<MapProps> = createHostComponent<MapProps>(
 
 Map.defaultProps = {
   scale: 16,
+  minScale: 3,
+  maxScale: 20,
   showLocation: false,
   layerStyle: 1,
   rotate: 0,
