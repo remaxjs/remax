7a4daf2c864d318d5f5924295447e8b9d1c35df9
Wei Zhu <yesmeck@gmail.com>
2021-04-22 19:38:31 +0800

    fix: 修复插件环境没有 getApp 导致的报错

--- packages/remax-runtime/src/createPageConfig.ts
+++ packages/remax-runtime/src/createPageConfig.ts
@@ -20,7 +20,12 @@ export function resetPageId() {
 }
 
 export default function createPageConfig(Page: React.ComponentType<any>, name: string) {
-  const app = getApp() as any;
+  let app: any;
+  try {
+    app = getApp();
+  } catch (e) {
+    app = null;
+  }
 
   const config: any = {
     data: {
@@ -74,7 +79,9 @@ export default function createPageConfig(Page: React.ComponentType<any>, name: s
       this.callLifecycle(Lifecycle.unload);
       this.unloaded = true;
       this.container.clearUpdate();
-      app._unmount(this);
+      if (app) {
+        app._unmount(this);
+      }
     },
 
     onTabItemTap(this: any, e: any) {
