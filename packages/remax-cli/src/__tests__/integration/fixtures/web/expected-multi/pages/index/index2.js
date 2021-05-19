0ccac7c1d1a0bdcc98fe7ac9f56f5be15acf0c4d
Wei Zhu <yesmeck@gmail.com>
2021-05-20 01:30:10 +0800

    test: update snapshot

--- packages/remax-cli/src/__tests__/integration/fixtures/web/expected-multi/pages/index/index2.js
+++ packages/remax-cli/src/__tests__/integration/fixtures/web/expected-multi/pages/index/index2.js
@@ -1,181 +1,143 @@
-(window['webpackJsonp'] = window['webpackJsonp'] || []).push([
-  [2],
-  [
-    ,
-    ,
-    /* 0 */ /* 1 */ /* 2 */
-    /***/ function (module, exports) {
-      module.exports = require('@remax/web/assets/normalize.css');
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[2],[
+/* 0 */,
+/* 1 */,
+/* 2 */
+/***/ (function(module, exports) {
 
-      /***/
-    },
-    /* 3 */
-    /***/ function (module, exports) {
-      module.exports = require('@remax/web/assets/app.css');
+module.exports = require("@remax/web/assets/normalize.css");
 
-      /***/
-    },
-    /* 4 */
-    /***/ function (module, __webpack_exports__, __webpack_require__) {
-      'use strict';
-      __webpack_require__.r(__webpack_exports__);
-      /* harmony import */ var _app_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(5);
-      /* harmony import */ var _app_css__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/ __webpack_require__.n(
-        _app_css__WEBPACK_IMPORTED_MODULE_0__
-      );
-      /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(6);
-      /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/ __webpack_require__.n(
-        react__WEBPACK_IMPORTED_MODULE_1__
-      );
+/***/ }),
+/* 3 */
+/***/ (function(module, exports) {
 
-      var App = function App(props) {
-        return props.children;
-      };
+module.exports = require("@remax/web/assets/app.css");
 
-      /* harmony default export */ __webpack_exports__['default'] = App;
+/***/ }),
+/* 4 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
 
-      /***/
-    },
-    /* 5 */
-    /***/ function (module, exports, __webpack_require__) {
-      // extracted by mini-css-extract-plugin
-      /***/
-    },
-    /* 6 */
-    /***/ function (module, exports) {
-      module.exports = require('react');
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _app_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(5);
+/* harmony import */ var _app_css__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_app_css__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(6);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_1__);
 
-      /***/
-    },
-    ,
-    /* 7 */ /* 8 */
-    /***/ function (module, exports) {
-      module.exports = require('remax');
 
-      /***/
-    },
-    /* 9 */
-    /***/ function (module, exports, __webpack_require__) {
-      // extracted by mini-css-extract-plugin
-      module.exports = {
-        app: 'app___2lhPP',
-        logo: 'logo___Al4Xb',
-        'logo-spin': 'logo-spin___3_EwH',
-        text: 'text___XEtZ5',
-        header: 'header___3qOk9',
-      };
 
-      /***/
-    },
-    /* 10 */
-    /***/ function (module, exports) {
-      module.exports = require('@remax/web');
+var App = function App(props) {
+  return props.children;
+};
 
-      /***/
-    },
-    /* 11 */
-    /***/ function (module, exports, __webpack_require__) {
-      module.exports = __webpack_require__(12);
+/* harmony default export */ __webpack_exports__["default"] = (App);
 
-      /***/
-    },
-    /* 12 */
-    /***/ function (module, __webpack_exports__, __webpack_require__) {
-      'use strict';
-      __webpack_require__.r(__webpack_exports__);
-      /* harmony import */ var _remax_web_assets_normalize_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(2);
-      /* harmony import */ var _remax_web_assets_normalize_css__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/ __webpack_require__.n(
-        _remax_web_assets_normalize_css__WEBPACK_IMPORTED_MODULE_0__
-      );
-      /* harmony import */ var _remax_web_assets_app_css__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(3);
-      /* harmony import */ var _remax_web_assets_app_css__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/ __webpack_require__.n(
-        _remax_web_assets_app_css__WEBPACK_IMPORTED_MODULE_1__
-      );
-      /* harmony import */ var _app__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(4);
-      /* harmony import */ var _index2_js__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(13);
-      /* harmony import */ var _remax_web__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(10);
-      /* harmony import */ var _remax_web__WEBPACK_IMPORTED_MODULE_4___default = /*#__PURE__*/ __webpack_require__.n(
-        _remax_web__WEBPACK_IMPORTED_MODULE_4__
-      );
+/***/ }),
+/* 5 */
+/***/ (function(module, exports, __webpack_require__) {
 
-      Object(_remax_web__WEBPACK_IMPORTED_MODULE_4__['bootstrapMpa'])({
-        appComponent: _app__WEBPACK_IMPORTED_MODULE_2__['default'],
-        pageComponent: _index2_js__WEBPACK_IMPORTED_MODULE_3__['default'],
-        page: {
-          route: 'pages/index/index2',
-          config: {},
-        },
-        appConfig: {
-          pages: ['pages/index/index', 'pages/index/index2'],
-          window: {
-            navigationBarTitleText: 'Remax Ali Template',
-            navigationBarBackgroundColor: '#282c34',
-          },
-        },
-        plugins: [],
-      });
+// extracted by mini-css-extract-plugin
 
-      /***/
-    },
-    /* 13 */
-    /***/ function (module, __webpack_exports__, __webpack_require__) {
-      'use strict';
-      __webpack_require__.r(__webpack_exports__);
-      /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(6);
-      /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/ __webpack_require__.n(
-        react__WEBPACK_IMPORTED_MODULE_0__
-      );
-      /* harmony import */ var remax__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(8);
-      /* harmony import */ var remax__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/ __webpack_require__.n(
-        remax__WEBPACK_IMPORTED_MODULE_1__
-      );
-      /* harmony import */ var _index_css_modules__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(9);
-      /* harmony import */ var _index_css_modules__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/ __webpack_require__.n(
-        _index_css_modules__WEBPACK_IMPORTED_MODULE_2__
-      );
+/***/ }),
+/* 6 */
+/***/ (function(module, exports) {
 
-      /* harmony default export */ __webpack_exports__['default'] = function (props) {
-        console.log(props);
-        return /*#__PURE__*/ react__WEBPACK_IMPORTED_MODULE_0__['createElement'](
-          remax__WEBPACK_IMPORTED_MODULE_1__['View'],
-          {
-            className: _index_css_modules__WEBPACK_IMPORTED_MODULE_2___default.a.app,
-            entry: true,
-          },
-          /*#__PURE__*/ react__WEBPACK_IMPORTED_MODULE_0__['createElement'](
-            remax__WEBPACK_IMPORTED_MODULE_1__['View'],
-            {
-              className: _index_css_modules__WEBPACK_IMPORTED_MODULE_2___default.a.header,
-            },
-            /*#__PURE__*/ react__WEBPACK_IMPORTED_MODULE_0__['createElement'](
-              remax__WEBPACK_IMPORTED_MODULE_1__['Image'],
-              {
-                src: 'https://gw.alipayobjects.com/mdn/rms_b5fcc5/afts/img/A*OGyZSI087zkAAAAAAAAAAABkARQnAQ',
-                className: _index_css_modules__WEBPACK_IMPORTED_MODULE_2___default.a.logo,
-                alt: 'logo',
-              }
-            ),
-            /*#__PURE__*/ react__WEBPACK_IMPORTED_MODULE_0__['createElement'](
-              remax__WEBPACK_IMPORTED_MODULE_1__['View'],
-              {
-                className: _index_css_modules__WEBPACK_IMPORTED_MODULE_2___default.a.text,
-              },
-              '\u7F16\u8F91 ',
-              /*#__PURE__*/ react__WEBPACK_IMPORTED_MODULE_0__['createElement'](
-                remax__WEBPACK_IMPORTED_MODULE_1__['Text'],
-                {
-                  className: _index_css_modules__WEBPACK_IMPORTED_MODULE_2___default.a.path,
-                },
-                'src/pages/index/index.js'
-              ),
-              ' \u5F00\u59CB'
-            )
-          )
-        );
-      };
+module.exports = require("react");
 
-      /***/
-    },
-  ],
-  [[11, 0]],
-]);
+/***/ }),
+/* 7 */,
+/* 8 */
+/***/ (function(module, exports) {
+
+module.exports = require("remax");
+
+/***/ }),
+/* 9 */
+/***/ (function(module, exports, __webpack_require__) {
+
+// extracted by mini-css-extract-plugin
+module.exports = {"app":"app___2lhPP","logo":"logo___Al4Xb","logo-spin":"logo-spin___3_EwH","text":"text___XEtZ5","header":"header___3qOk9"};
+
+/***/ }),
+/* 10 */
+/***/ (function(module, exports) {
+
+module.exports = require("@remax/web");
+
+/***/ }),
+/* 11 */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(12);
+
+
+/***/ }),
+/* 12 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _remax_web_assets_normalize_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(2);
+/* harmony import */ var _remax_web_assets_normalize_css__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_remax_web_assets_normalize_css__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _remax_web_assets_app_css__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(3);
+/* harmony import */ var _remax_web_assets_app_css__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_remax_web_assets_app_css__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var _app__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(4);
+/* harmony import */ var _index2_js__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(13);
+/* harmony import */ var _remax_web__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(10);
+/* harmony import */ var _remax_web__WEBPACK_IMPORTED_MODULE_4___default = /*#__PURE__*/__webpack_require__.n(_remax_web__WEBPACK_IMPORTED_MODULE_4__);
+
+
+
+
+
+Object(_remax_web__WEBPACK_IMPORTED_MODULE_4__["bootstrapMpa"])({
+  appComponent: _app__WEBPACK_IMPORTED_MODULE_2__["default"],
+  pageComponent: _index2_js__WEBPACK_IMPORTED_MODULE_3__["default"],
+  page: {
+    "route": "pages/index/index2",
+    "config": {}
+  },
+  appConfig: {
+    "pages": ["pages/index/index", "pages/index/index2"],
+    "window": {
+      "navigationBarTitleText": "Remax Ali Template",
+      "navigationBarBackgroundColor": "#282c34"
+    }
+  },
+  plugins: []
+});
+
+/***/ }),
+/* 13 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(6);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var remax__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(8);
+/* harmony import */ var remax__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(remax__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var _index_css_modules__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(9);
+/* harmony import */ var _index_css_modules__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(_index_css_modules__WEBPACK_IMPORTED_MODULE_2__);
+
+
+
+/* harmony default export */ __webpack_exports__["default"] = (function (props) {
+  console.log(props);
+  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__["createElement"](remax__WEBPACK_IMPORTED_MODULE_1__["View"], {
+    className: _index_css_modules__WEBPACK_IMPORTED_MODULE_2___default.a.app,
+    entry: true
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__["createElement"](remax__WEBPACK_IMPORTED_MODULE_1__["View"], {
+    className: _index_css_modules__WEBPACK_IMPORTED_MODULE_2___default.a.header
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__["createElement"](remax__WEBPACK_IMPORTED_MODULE_1__["Image"], {
+    src: "https://gw.alipayobjects.com/mdn/rms_b5fcc5/afts/img/A*OGyZSI087zkAAAAAAAAAAABkARQnAQ",
+    className: _index_css_modules__WEBPACK_IMPORTED_MODULE_2___default.a.logo,
+    alt: "logo"
+  }), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__["createElement"](remax__WEBPACK_IMPORTED_MODULE_1__["View"], {
+    className: _index_css_modules__WEBPACK_IMPORTED_MODULE_2___default.a.text
+  }, "\u7F16\u8F91 ", /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__["createElement"](remax__WEBPACK_IMPORTED_MODULE_1__["Text"], {
+    className: _index_css_modules__WEBPACK_IMPORTED_MODULE_2___default.a.path
+  }, "src/pages/index/index.js"), " \u5F00\u59CB")));
+});
+
+/***/ })
+],[[11,0]]]);
