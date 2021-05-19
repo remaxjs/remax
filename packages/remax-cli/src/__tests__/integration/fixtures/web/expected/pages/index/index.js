4bbca2541e381825ef224c284e5d271180eda7ba
Wei Zhu <yesmeck@gmail.com>
2021-05-20 01:03:26 +0800

    test: update snapshot

--- packages/remax-cli/src/__tests__/integration/fixtures/web/expected/pages/index/index.js
+++ packages/remax-cli/src/__tests__/integration/fixtures/web/expected/pages/index/index.js
@@ -5,7 +5,7 @@
 
 "use strict";
 __webpack_require__.r(__webpack_exports__);
-/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(4);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(6);
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
 /* harmony import */ var remax__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(9);
 /* harmony import */ var remax__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(remax__WEBPACK_IMPORTED_MODULE_1__);
