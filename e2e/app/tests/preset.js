79c3eaf6383e9a3de023775b01699b4448c0ee73
hehe <xpc1993@gmail.com>
2021-05-28 23:09:30 +0800

    chore: upgrade jest to 27 (#1587)
    * chore: upgrade jest to 27
    
    * chore: upgrade jest-puppeteer
    
    * chore: use jasmine2 in e2e

--- e2e/app/tests/preset.js
+++ e2e/app/tests/preset.js
@@ -3,5 +3,6 @@ const ts_preset = require('ts-jest/jest-preset');
 const puppeteer_preset = require('jest-puppeteer/jest-preset');
 
 module.exports = merge.recursive(ts_preset, puppeteer_preset, {
+  testRunner: 'jest-jasmine2',
   setupFilesAfterEnv: ['./tests/setup.js'],
 });
