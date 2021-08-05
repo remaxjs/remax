3d06f6c639ec7380fe2c28f8c8c24c8dcf16702d
sydeEvans <evan.wangjue@qq.com>
2021-08-05 20:22:14 +0800

    feat: 调整remax/one的button组件dom结构

--- e2e/app/src/pages/index/index.js
+++ e2e/app/src/pages/index/index.js
@@ -1,8 +1,24 @@
 import * as React from 'react';
-import { View } from 'remax/one';
+import { View, Text, Button, Image } from 'remax/one';
 
 export default class Page extends React.Component {
   render() {
-    return <View>hello</View>;
+    return (
+      <View>
+        <View>
+          <Button style={{background: "#0074D9", color: "white"}}>hello</Button>
+        </View>
+
+        <View>
+          <Text>hello</Text>
+        </View>
+
+        <View>
+          <Image
+            mode="aspectFit"
+            src="https://gw.alipayobjects.com/mdn/rms_b5fcc5/afts/img/A*1NHAQYduQiQAAAAAAAAAAABkARQnAQ" />
+        </View>
+      </View>
+    );
   }
 }
