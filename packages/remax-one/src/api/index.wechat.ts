37cef4ed80286ddd850a1bb8d613d0dc25eb01d3
Wei Zhu <yesmeck@gmail.com>
2021-11-23 14:11:52 +0800

    refactor(one): 不再从 @remax/wechat 引用 api (#1771)
    Co-authored-by: 诸威 <zhuwei.zw@bytedance.com>

--- packages/remax-one/src/api/index.wechat.ts
+++ packages/remax-one/src/api/index.wechat.ts
@@ -1,1 +1,9 @@
-export { navigateTo, navigateBack, redirectTo, reLaunch, switchTab } from '@remax/wechat';
+import { promisify } from '@remax/framework-shared';
+
+declare const wx: any;
+
+export const navigateTo = promisify(wx.navigateTo);
+export const navigateBack = promisify(wx.navigateBack);
+export const redirectTo = promisify(wx.redirectTo);
+export const reLaunch = promisify(wx.reLaunch);
+export const switchTab = promisify(wx.switchTab);
