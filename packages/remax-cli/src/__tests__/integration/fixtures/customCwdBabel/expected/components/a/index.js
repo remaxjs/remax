e802d9ca549e8d63b94cd43b47ac3aab55537a45
watsonhaw <348748267@qq.com>
2021-12-17 19:41:32 +0800

    test: 更新单元测试文件

---
+++ packages/remax-cli/src/__tests__/integration/fixtures/customCwdBabel/expected/components/a/index.js
@@ -0,0 +1,11 @@
+require('./../../runtime.js');
+(my["webpackJsonp"] = my["webpackJsonp"] || []).push([[3],{
+
+/***/ 12:
+/***/ (function(module, exports) {
+
+Component({});
+
+/***/ })
+
+},[[12,0]]]);
---
+++ packages/remax-cli/src/__tests__/integration/fixtures/namespaceAttribute/expected/components/native-component/index.js
@@ -0,0 +1,14 @@
+require('./../../runtime.js');
+(my["webpackJsonp"] = my["webpackJsonp"] || []).push([[3],{
+
+/***/ 14:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+Component({});
+/* harmony default export */ __webpack_exports__["default"] = ({});
+
+/***/ })
+
+},[[14,0]]]);
