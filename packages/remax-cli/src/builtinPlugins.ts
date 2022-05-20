8e741d8bc201d9c05a3190efa3f4fcb707ba245c
sydeEvans <evan.wangjue@qq.com>
2022-05-20 14:53:36 +0800

    fix: 关闭 react-devtool 默认集成

--- packages/remax-cli/src/builtinPlugins.ts
+++ packages/remax-cli/src/builtinPlugins.ts
@@ -1,5 +1,5 @@
 import errorScreen from '@remax/plugin-error-screen';
-import devtools from '@remax/plugin-devtools';
+// import devtools from '@remax/plugin-devtools';
 import type { Options, Plugin } from '@remax/types';
 
 export const builtinPlugins = (
@@ -8,18 +8,18 @@ export const builtinPlugins = (
   optionKey: string;
   init: (...args: any[]) => Plugin;
 }> => {
-  const isDev = process.env.NODE_ENV !== 'production';
+  // const isDev = process.env.NODE_ENV !== 'production';
   const plugins = [
     {
       optionKey: 'errorScreen',
       init: errorScreen,
     },
   ];
-  if (options.target !== 'web' && isDev && options.devtools) {
-    plugins.push({
-      optionKey: 'devtools',
-      init: devtools,
-    });
-  }
+  // if (options.target !== 'web' && isDev && options.devtools) {
+  //   plugins.push({
+  //     optionKey: 'devtools',
+  //     init: devtools,
+  //   });
+  // }
   return plugins;
 };
